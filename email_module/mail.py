58232756dec0ca5186892bc038c5eca46eda7809
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-10-02 23:15:12 +0530

    added env varibles

--- email_module/mail.py
+++ email_module/mail.py
@@ -1,15 +1,16 @@
+import os
 import smtplib
+from dotenv import load_dotenv
 
-my_email = "madoldsniper@gmail.com"
-to_email = "madoldsniper@gmail.com"
-user = "madoldsniper@gmail.com"
-password = "envqsfsmqwwsmyai"
+# load all env variables
+
+load_dotenv()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port=465) as connection:
     # secure the connection(packets sent using ttls)
     connection.ehlo()
-    connection.login(user=user, password=password)
+    connection.login(user=os.getenv('user'), password=os.getenv('password'))
     connection.sendmail(
-        from_addr=my_email,
-        to_addrs=to_email,
+        from_addr=os.getenv('my_email'),
+        to_addrs=os.getenv('my_email'),
         msg="Subject: This is a test subject\n\nTest Message")
