72bd7f96acc9706f0df86fbf9edd5c7762b4b229
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-10-04 23:47:18 +0530

    checking if ISS is visible or not

--- location/main.py
+++ location/main.py
@@ -7,6 +7,7 @@ response.raise_for_status()
 
 iss_data = response.json()
 
+# my location
 day_dict = {
     'lat': 12.976750,
     'lng': 77.575279,
@@ -15,3 +16,6 @@ day_dict = {
 day_info = requests.get("https://api.sunrise-sunset.org/json", params=day_dict)
 sunrise = day_info.json()['results']['sunrise']
 sunset = day_info.json()['results']['sunset']
+
+# check if iss is at your location post sunset and before sunrise
+print(sunrise, sunset)
